1f3a9cfe2a9b7e5124d75be93375034c6580b6cf
Carlos G. Rodriguez <carlosro.ec@gmail.com>
2020-01-12 21:35:20 -0500

    B4: search using promises

--- b4/lib/search.js
+++ b4/lib/search.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const request = require('request');
+const rp = require('request-promise');
 
 module.exports = (app, es) => {
     const url = `http://${es.host}:${es.port}/${es.book_index}/book/_search`;
@@ -34,5 +35,48 @@ module.exports = (app, es) => {
 
             res.status(200).json(esResBody.hits.hits.map(({_source}) => _source));
         });
+    });
+
+    app.get('/api/suggest/:field/:query', (req, res) => {
+        const esReqBody = {
+            size: 0,
+            suggest: {
+                suggestions: {
+                    text: req.params.query,
+                    term: {
+                        field: req.params.field,
+                        suggest_mode: 'always'
+                    }
+                }
+            }
+        };
+
+        // const options = {url, json: true, body: esReqBody};
+
+        // const promise = new Promise((resolve, reject) => {
+        //     request.get(options, (err, esRes, esResBody) => {
+        //         if (err) {
+        //             reject({error: err});
+
+        //             return;
+        //         }
+
+        //         if (esRes.statusCode !== 200) {
+        //             reject({error: esResBody});
+
+        //             return;
+        //         }
+
+        //         resolve(esResBody);
+        //     });
+        // });
+
+        // promise
+        //     .then(esResBody => res.status(200).json(esResBody.suggest.suggestions))
+        //     .catch(({error}) => res.status(error.status || 502).json(error));
+
+        rp({url, json: true, body: esReqBody})
+            .then(esResBody => res.status(200).json(esResBody.suggest.suggestions))
+            .catch(({error}) => res.status(error.status || 502).json(error));
     });
 }
