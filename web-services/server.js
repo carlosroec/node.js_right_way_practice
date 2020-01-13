2eb220c6056c64301186c26ddd3393c383136f9c
Carlos G. Rodriguez <carlosro.ec@gmail.com>
2020-01-12 21:20:01 -0500

    B4: adding search

---
+++ web-services/server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World\n');
+});
+
+server.listen(60800, () => console.log('Ready!'));
