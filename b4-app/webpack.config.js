7542a7c1b7ecdf7500895e4dcc9761ead9dd0bac
Carlos G. Rodriguez <carlosro.ec@gmail.com>
2020-02-02 10:07:24 -0500

    FE with Handlebars

--- b4-app/webpack.config.js
+++ b4-app/webpack.config.js
@@ -1,26 +1,41 @@
 'use strict';
 
+const webpack = require('webpack');
 const path = require('path');
 const distDir = path.resolve(__dirname, 'dist');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './entry.js',
+    entry: './app/index.ts',
     output: {
         filename: 'bundle.js',
         path: distDir
     },
     devServer: {
         contentBase: distDir,
-        port: 60800
+        port: 60800,
+        proxy: {
+            '/api': 'http://localhost:60702',
+            '/es': {
+                target: 'http://localhost:9201',
+                pathRewrite: {'^/es': ''}
+            }
+        }
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Better Book Blundle Builder'
         }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        })
     ],
     module: {
         rules: [{
+            test: /\.ts$/,
+            loader: 'ts-loader'
+        },{
             test: /\.css$/,
             use: ['style-loader', 'css-loader']
         },{
